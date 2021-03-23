3e871e09b117316d406e15612ed33ca6620ab0a6
Anastasia Korol <anastasiia.korol@abbyy.com>
2021-03-23 19:11:31 +0300

    test tree looks better but still doesn't open in GUI and too any requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.apache.jmeter.control.gui.TestPlanGui;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
 import org.apache.jmeter.report.config.ConfigurationException;
@@ -5,6 +6,7 @@ import org.apache.jmeter.report.dashboard.GenerationException;
 import org.apache.jmeter.report.dashboard.ReportGenerator;
 import org.apache.jmeter.reporters.ResultCollector;
 import org.apache.jmeter.save.SaveService;
+import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.threads.ThreadGroup;
 import org.apache.jorphan.collections.HashTree;
@@ -22,14 +24,17 @@ public class Main {
 
         HTTPSamplerProxy httpSampler = TestPlanUtils.getHttpSampler("www.google.com", 80, "/", HttpMethod.GET);
 
-        ThreadGroup threadGroup = TestPlanUtils.getSimpleThreadGroup("Test", 1, 1);
+        ThreadGroup threadGroup = TestPlanUtils.getSimpleThreadGroup("Test", 1, 1, httpSampler);
 
         // Test plan
         TestPlan testPlan = new TestPlan("MY TEST PLAN");
         testPlan.setEnabled(true);
+        testPlan.setProperty(TestElement.TEST_CLASS, TestPlan.class.getName());
+        testPlan.setProperty(TestElement.GUI_CLASS, TestPlanGui.class.getName());
 
         hashTree.add(testPlan);
-        hashTree.add(testPlan, threadGroup).add(threadGroup, httpSampler);
+        HashTree threadGroupHashTree = hashTree.add(testPlan, threadGroup);
+        threadGroupHashTree.add(httpSampler);
 
         // Generate the JMX
         SaveService.saveTree(hashTree, new FileOutputStream("sample.jmx"));
--- src/main/java/TestPlanUtils.java
+++ src/main/java/TestPlanUtils.java
@@ -1,18 +1,26 @@
 import org.apache.jmeter.control.LoopController;
+import org.apache.jmeter.control.gui.LoopControlPanel;
+import org.apache.jmeter.protocol.http.control.gui.HttpTestSampleGui;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
+import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.threads.ThreadGroup;
+import org.apache.jmeter.threads.gui.ThreadGroupGui;
 
 public class TestPlanUtils {
-    public static ThreadGroup getSimpleThreadGroup(String name, Integer threads, Integer rampUpSeconds) {
+    public static ThreadGroup getSimpleThreadGroup(String name, Integer threads, Integer rampUpSeconds, HTTPSamplerProxy sampler) {
         LoopController loopController = new LoopController();
         loopController.setLoops(1);
         loopController.setFirst(true);
+        loopController.setProperty(TestElement.TEST_CLASS, LoopController.class.getName());
+        loopController.setProperty(TestElement.GUI_CLASS, LoopControlPanel.class.getName());
         loopController.initialize();
 
         ThreadGroup threadGroup = new ThreadGroup();
         threadGroup.setNumThreads(threads);
         threadGroup.setRampUp(rampUpSeconds);
         threadGroup.setSamplerController(loopController);
+        threadGroup.setProperty(TestElement.TEST_CLASS, ThreadGroup.class.getName());
+        threadGroup.setProperty(TestElement.GUI_CLASS, ThreadGroupGui.class.getName());
 
         return threadGroup;
     }
@@ -23,6 +31,8 @@ public class TestPlanUtils {
         httpSampler.setPort(port);
         httpSampler.setPath(path);
         httpSampler.setMethod(method.toString());
+        httpSampler.setProperty(TestElement.TEST_CLASS, HTTPSamplerProxy.class.getName());
+        httpSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());
 
         return httpSampler;
     }
